b58b95d637a8008a0dd71beec6ffab3c557afd87
jsalgueroibarrola <82667763+jsalgueroibarrola@users.noreply.github.com>
2024-12-01 22:50:39 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,46 @@
+fun part1(input: List<String>): Int {
+    val (leftList, rightList) = parseInputToLists(input=input)
+
+    val sortedLeft = leftList.sorted()
+    val sortedRight = rightList.sorted()
+
+    val result = sortedLeft.mapIndexed { index, left ->
+        calculateDifference(left=left, right=sortedRight[index])
+    }.sum()
+
+    return result
+}
+
+fun part2(input: List<String>): Int {
+    val (leftList, rightList) = parseInputToLists(input=input)
+
+    val frequencyMap = mutableMapOf<Int, Int>()
+    rightList.forEach { right ->
+        frequencyMap[right] = frequencyMap.getOrDefault(right, 0) + 1
+    }
+
+    val result = leftList.sumOf { left ->
+        left * frequencyMap.getOrDefault(left, 0)
+    }
+    return result
+}
+
+private fun parseInputToLists(input: List<String>): Pair<List<Int>, List<Int>> {
+    return input
+        .map {
+            val parts = it.split("\\s+".toRegex())
+            parts[0].toInt() to parts[1].toInt()
+        }.unzip()
+}
+
+fun calculateDifference(left: Int, right: Int): Int{
+    if(left > right){
+        return left - right
+    }
+    return right - left
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
